8bd15c670d044b9b40f0ea4edc587ae389496d45
quyq <dummyfk@gmail.com>
2024-02-17 15:36:32 -0500

    Add credential json save & load

--- blink.py
+++ blink.py
@@ -1,10 +1,23 @@
 import asyncio
 from aiohttp import ClientSession
 from blinkpy.blinkpy import Blink
+from blinkpy.auth import Auth
+from blinkpy.helpers.util import json_load
+
+import os
+
+cred_file = "./cred.json"
 
 async def start():
-    blink = Blink(session=ClientSession())
-    await blink.start()
+    if os.path.isfile(cred_file):
+        blink = Blink()
+        auth = Auth(await json_load(cred_file))
+        blink.auth = auth
+        await blink.start()
+    else:
+        blink = Blink(session=ClientSession())
+        await blink.start()
+        if blink: await blink.save(cred_file)
     return blink
 
 blink = asyncio.run(start())
