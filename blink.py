8c4124ab31403728b3156bf4b78a4e3e9f3d44ba
quyq <dummyfk@gmail.com>
2024-03-03 15:19:30 -0500

    Add sync.arm status check

--- blink.py
+++ blink.py
@@ -21,6 +21,7 @@ async def start():
     return blink
 
 async def pull(blink, camera):
+    await camera.async_arm(True)  # Arm a sync. Same as: await blink.sync[camera.name].async_arm(True)
     await blink.refresh(force=True)  # force a cache update USE WITH CAUTION
     data_jpg = camera.image_from_cache  # bytes-like image object (jpg)
     data_vid = camera.video_from_cache  # bytes-like video object (mp4)
@@ -28,6 +29,13 @@ async def pull(blink, camera):
     else: print("No jpg data")
     if data_vid: print("Cached %d bytes as video" % len(data_vid))
     else: print("No video data")
+    #await camera.image_to_file('./image.jpg')
+    #await camera.video_to_file('./video.mp4')
+    sync = blink.sync[camera.name]
+    print(f"{sync.name} status: {sync.arm}")
+
+    #https://rest-u056.immedia-semi.com/api/v3/media/accounts/222219/networks/239481/owl/290887/thumbnail/thumbnail.jpg?ts=1669122404&ext=
+    #{base_url}/api/v3/accounts/{account_id}/networks/{network_id}/sync_modules/{sync_id}/local_storage/manifest/{manifest_id}/clip/request/
 
 async def main():
     blink = await start()
